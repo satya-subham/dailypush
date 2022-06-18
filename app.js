d6a678a53343430d9cb81386e695cc0c4dd76a44
satya-subham <satyasubham85@gmail.com>
2022-06-18 21:08:25 +0530

    assignment

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-let a = 13;
-let firstDigit = Math.floor(a/10);
-let lastDigit = a%10;
-console.log(firstDigit);
-console.log(lastDigit);
+let number = 407;
+let firstDigit = Math.floor(number/100);
+let secondDigit = Math.floor((number%100)/10);
+let lastDigit = ((number%100)%10);
+if(firstDigit**3 + secondDigit**3 + lastDigit**3 == number){
+    console.log("It's an armstrong number");
+}else{
+    console.log("it's not an armstrong number");
+}
+
+
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 <img src="q1.png alt"" />
 <img src="q2.png alt"" />
 <img src="q3.png alt"" />
+<img src="q4.png alt"" />
