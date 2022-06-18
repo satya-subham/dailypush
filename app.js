c849634edb4cccfc5738de45c7a6d2c7b8819f08
satya-subham <satyasubham85@gmail.com>
2022-06-18 20:34:04 +0530

    assignment

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-let sum  = 0;
-let a = 1;
-let b = 20;
-for(let i=a; i<=b; i++){
-    sum = sum+i;
-}
-console.log(sum);
+let a = 13;
+let firstDigit = Math.floor(a/10);
+let lastDigit = a%10;
+console.log(firstDigit);
+console.log(lastDigit);
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 <img src="q1.png alt"" />
 <img src="q2.png alt"" />
+<img src="q3.png alt"" />
